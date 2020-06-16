1d81094586275dc6f6922211389f85bb1e6cb29f
EC2 Default User <ec2-user@ip-172-16-8-74.eu-central-1.compute.internal>
2020-06-16 11:08:41 +0000

    finalize project

--- Project_Plagiarism_Detection/source_sklearn/train.py
+++ Project_Plagiarism_Detection/source_sklearn/train.py
@@ -7,7 +7,7 @@ import pandas as pd
 from sklearn.externals import joblib
 
 ## TODO: Import any additional libraries you need to define a model
-
+from sklearn.ensemble import RandomForestClassifier
 
 # Provided model load function
 def model_fn(model_dir):
@@ -39,6 +39,8 @@ if __name__ == '__main__':
     parser.add_argument('--data-dir', type=str, default=os.environ['SM_CHANNEL_TRAIN'])
     
     ## TODO: Add any additional arguments that you will need to pass into your model
+    parser.add_argument("--num", type=int, default=100)
+    parser.add_argument("--seed", type=int, default=0)
     
     # args holds all passed-in arguments
     args = parser.parse_args()
@@ -56,11 +58,11 @@ if __name__ == '__main__':
     
 
     ## TODO: Define a model 
-    model = None
+    model = RandomForestClassifier(n_estimators=args.num,random_state=args.seed)
     
     
     ## TODO: Train the model
-    
+    model.fit(train_x, train_y)
     
     
     ## --- End of your code  --- ##
